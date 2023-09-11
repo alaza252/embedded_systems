bfdd30088d4e26149944adaf670d8ece3926d65e
Schulte, Drew (S&T-Student) <dnsdyy@mst.edu>
2023-09-11 14:51:05 -0500

    addedsoruceproject

---
+++ Expirement1_Aloof/Expirement1_Aloof/main.c
@@ -0,0 +1,18 @@
+/*
+ * Expirement1_Aloof.c
+ *
+ * Created: 9/11/2023 2:37:32 PM
+ * Author : dnsdyy
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
