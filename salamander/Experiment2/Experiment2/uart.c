d586c197a88140698a7b7adcffd71f702242170b
Lavender Shannon <jdsfz4@mst.edu>
2023-09-28 12:24:33 -0500

    Added uart_receive_nb and implemented blocking and non-blocking versions

--- salamander/Experiment2/Experiment2/uart.c
+++ salamander/Experiment2/Experiment2/uart.c
@@ -16,8 +16,28 @@ uint8_t uart_transmit(volatile UART_t *addr, uint8_t send_val)
 {
 	return 0;
 }
+uint8_t uart_receive(volatile UART_t *addr, uint8_t *rcvd_val) {
+	uint8_t status;
+	uint16_t timeout_counter = 0;
+	do {
+		status = uart_receive_nb(addr, rcvd_val);
+		timeout_counter++;
+	} while ((status & UART_RECEIVE_NB_INCOMPLETE_MASK) != 0 && timeout_counter != 0)
+
+	if ((status & UART_RECEIVE_NB_INCOMPLETE_MASK) != 0) {
+		// If the receive is still incomplete after the loop, a timeout must have occurred
+		return UART_RECEIVE_TIMEOUT_MASK;
+	}
+	return status;
+}
 
 uint8_t uart_receive_nb(volatile UART_t *addr, uint8_t *rcvd_val)
 {
-	return 0;
+	if ((addr->UCSRA & (1 << RXC)) == 0) { // TODO is 1 << RXC correct? Do we need to include avr or something?
+		// receive complete not set,
+		return UART_RECEIVE_NB_INCOMPLETE_MASK;
+	}
+	// receive is complete, so let's read UDR. When we read UDR, the RXC flag will automatically be cleared
+	*rcvd_val = addr->UDR;
+	return UART_RECEIVE_NB_SUCCESS;
 }
--- salamander/Experiment2/Experiment2/uart.h
+++ salamander/Experiment2/Experiment2/uart.h
@@ -23,10 +23,39 @@ typedef struct UART
 #define UART1 ((volatile UART_t*) &UCSR1A)
 #define UART2 ((volatile UART_t*) &UCSR2A)
 
+#define UART_RECEIVE_SUCCESS 0
+/** The mask value used in uart_receive_nb for an incomplete receive*/
+#define UART_RECEIVE_INCOMPLETE_MASK (1 << 0)
+/** The mask value used in uart_receive for a timeout while trying to receive*/
+#define UART_RECEIVE_TIMEOUT_MASK (1 << 0)
+
+#define ASYNC_MODE (0 << 6)
+#define SYNC_MODE (1 << 6)
+#define SPI_MODE (3 << 6)
+
+#define NO_PARITY (0 << 4)
+#define EVEN_PARITY (2 << 4)
+#define ODD_PARITY (3 << 4)
+
+#define ONE_STOP_BIT (0 << 3)
+#define TWO_STOP_BITS (1 << 3)
+
 void uart_init(volatile UART_t *addr, uint32_t baud_rate);
 
 uint8_t uart_transmit(volatile UART_t *addr, uint8_t send_val);
 
+/**
+ * If the returned value is UART_RECEIVE_NB_SUCCESS, rcvd_val will be set to contain the received byte.
+ * If the returned value is not UART_RECEIVE_NB_SUCCESS, it may have one or multiple bits set indicating the error(s) that occurred during receive
+ *
+ * This function blocks until
+ */
+uint8_t uart_receive(volatile UART_t *addr, uint8_t *rcvd_val);
+
+/**
+ * If the returned value is UART_RECEIVE_NB_SUCCESS, rcvd_val will be set to contain the received byte.
+ * If the returned value is not UARt_RECEIVE_NB_SUCCESS, it may have one or multiple bits set indicating the error(s) that occurred during receive
+ */
 uint8_t uart_receive_nb(volatile UART_t *addr, uint8_t *rcvd_val);
 
 
